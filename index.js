ae599273011ccc4b737b7a19418559c53891e9fb
brianpocasangre <brianpocasangre1@gmail.com>
2024-06-10 13:22:24 -0700

    add ejs to index.ejs file and struggle with api fetch

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
-import express from "express";
-import bodyParser from "body-parser";
-import axios from "axios";
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
 
 const app = express();
 const port = 3000;
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Step 1: Make sure that when a user visits the home page,
 //   it shows a random activity.You will need to check the format of the
 //   JSON data from response.data and edit the index.ejs file accordingly.
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   try {
-    const response = await axios.get("https://bored-api.appbrewery.com/random");
+    const response = await axios.get('https://bored-api.appbrewery.com/random');
     const result = response.data;
-    res.render("index.ejs", { data: result });
+    res.render('index.ejs', { data: result });
   } catch (error) {
-    console.error("Failed to make request:", error.message);
-    res.render("index.ejs", {
+    console.error('Failed to make request:', error.message);
+    res.render('index.ejs', {
       error: error.message,
     });
   }
 });
 
-app.post("/", async (req, res) => {
-  console.log(req.body);
+app.post('/', async (req, res) => {
+  console.log(req.body.participants);
 
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
   // sure you're passing both the type and participants queries.
+
+  try {
+    const response = await axios.get(
+      `https://bored-api.appbrewery.com/filter?type=${req.body.type}&participants=${req.body.participants}`
+    );
+    const result = response.data;
+    res.render('index.ejs', {
+      data: result[Math.floor(Math.random() * result.length)],
+    });
+  } catch (error) {
+    console.error('Failed to make request:', error.message);
+    res.render('index.ejs', {
+      error:
+        'No Activities That Match Your Criteria, Please Try Another Option.',
+    });
+  }
+
   // Render the index.ejs file with a single *random* activity that comes back
   // from the API request.
   // Step 3: If you get a 404 error (resource not found) from the API request.
